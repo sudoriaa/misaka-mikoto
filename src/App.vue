61c966229919fecfabab29d1ea73fef35ea45046
苏多莉亚 <3078037152@qq.com>
2023-12-14 19:08:47 +0800

    构建Vue框架

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Misaka />
+  </div>
+</template>
+
+<script>
+import Misaka from './components/Misaka.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Misaka
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Misaka.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="misaka">
+      <h1>{{ title }}</h1>
+      <img :src="image" alt="Misaka Mikoto" />
+      <p>{{ description }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        title: '御坂美琴应援网页',
+        image: 'path/to/misaka/image.jpg', // 替换为御坂美琴的图片路径
+        description: '御坂美琴是《魔法禁书目录》和《科学超电磁炮》的主要角色之一，是一名能力者，代号为“电磁炮”。'
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .misaka {
+    text-align: center;
+  }
+  .misaka img {
+    width: 300px;
+    height: auto;
+  }
+  </style>
+  
